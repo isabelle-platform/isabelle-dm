6dbabcf0b12942dd4cf91c053ef18d9bf2512811
Maxim Menshikov <maxim@menshikov.org>
2023-08-29 22:56:24 +0100

    Remove mentees

--- src/data_model/mentee_list.rs
+++
@@ -1,8 +0,0 @@
-use yew::prelude::*;
-use crate::data_model::mentee::Mentee;
-use serde::{Deserialize, Serialize};
-
-#[derive(Serialize, Deserialize, PartialEq, Clone, Debug, Properties)]
-pub struct MenteeList {
-    pub mentees: Vec<Mentee>,
-}
--- src/data_model/mod.rs
+++ src/data_model/mod.rs
@@ -1,6 +1,4 @@
 pub mod del_param;
-pub mod mentee;
-pub mod mentee_list;
 pub mod schedule_entry;
 pub mod schedule_list;
 pub mod item;
