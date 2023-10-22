94fd5c603ca68fce3970858fd4504179f0583af0
Maxim Menshikov <maxim@menshikov.org>
2023-10-23 00:46:26 +0100

    login->username

--- src/data_model/login_user.rs
+++ src/data_model/login_user.rs
@@ -5,7 +5,7 @@ use yew::prelude::*;
 #[derive(Serialize, Deserialize, PartialEq, Clone, Debug, Properties)]
 pub struct LoginUser {
     #[serde(default = "unset_str")]
-    pub login: String,
+    pub username: String,
 
     #[serde(default = "unset_str")]
     pub password: String,
