e44b1e363d9b97f7b2d78656ce70a16f96fef538
Maxim Menshikov <maxim@menshikov.org>
2024-09-08 09:57:49 +0100

    [#3] Reformat source

--- src/data_model/data_object_action.rs
+++ src/data_model/data_object_action.rs
@@ -21,7 +21,7 @@
  * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
  * DEALINGS IN THE SOFTWARE.
  */
-use serde::{Serialize, Deserialize};
+use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, PartialEq, Clone, Debug)]
 #[repr(C)]
